9e2c1048dfb1bd25fe072b19e0b6fe6fed07e91e
Tanel Paal <tanelpaal@gmail.com>
2025-05-06 18:32:25 +0300

    Removed commented code

--- App.DAL.EF/Repositories/PersonPaintsRepository.cs
+++ App.DAL.EF/Repositories/PersonPaintsRepository.cs
@@ -11,41 +11,4 @@ public class PersonPaintsRepository : BaseBaseRepository<App.DAL.DTO.PersonPaint
     public PersonPaintsRepository(AppDbContext repositoryDbContext) : base(repositoryDbContext, new PersonPaintsUOWMapper())
     {
     }
-    
-    /*public override async Task<IEnumerable<App.DAL.DTO.PersonPaints>> AllAsync(Guid userId = default!)
-    {
-        return (await RepositoryDbSet
-            .Include(p => p.Paint)
-            .Include(p => p.Person)
-            .Where(p => p.UserId == userId)
-            .ToListAsync()).Select(e => UOWMapper.Map(e)!);
-    }
-
-    public override async Task<App.DAL.DTO.PersonPaints?> FindAsync(Guid id, Guid userId = default!)
-    {
-        var entity =  await RepositoryDbSet
-            .Include(p => p.Paint)
-            .Include(p => p.Person)
-            .Include(p => p.MiniPaintSwatches)
-            .FirstOrDefaultAsync(p => p.Id == id && p.UserId == userId);
-        
-        return UOWMapper.Map(entity);
-    }
-
-    public async Task<bool> IsOwnedByUserAsync(Guid id, Guid userId)
-    {
-        return await RepositoryDbSet
-            .AnyAsync(p => p.Id == id && p.UserId == userId);
-    }
-
-    public async Task RemoveAsync(Guid id, Guid userId)
-    {
-        var entity = await RepositoryDbSet
-            .FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
-            
-        if (entity != null)
-        {
-            RepositoryDbSet.Remove(entity);
-        }
-    }*/
 }

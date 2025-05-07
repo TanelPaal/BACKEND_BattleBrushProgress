91c3d786c544a399d036aed95511b035a6fcffd5
Tanel Paal <tanelpaal@gmail.com>
2025-05-07 10:06:59 +0300

    account testing

--- App.Tests/Integrations/Api/IdentityTests.cs
+++ App.Tests/Integrations/Api/IdentityTests.cs
@@ -1,18 +1,20 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
+using System.Net.Http.Json;
 using App.DTO.v1.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.VisualStudio.Web.CodeGeneration.Design;
 using Xunit;
+using Xunit.Extensions.Ordering;
 
 
 namespace App.Tests.Integrations.Api;
 
+[Collection("Sequential")]
 public class IdentityTests: IClassFixture<CustomWebApplicationFactory<Program>>
 {
     private readonly HttpClient _client;
     private readonly CustomWebApplicationFactory<Program> _factory;
-
 
     public IdentityTests(CustomWebApplicationFactory<Program> factory)
     {
@@ -25,7 +27,7 @@ public class IdentityTests: IClassFixture<CustomWebApplicationFactory<Program>>
     
     
     [Fact]
-    public async Task Register_User()
+    public async Task Register_New_User()
     {
         // Arrange
         var registrationData = new Register()
@@ -41,6 +43,154 @@ public class IdentityTests: IClassFixture<CustomWebApplicationFactory<Program>>
 
         // Assert
         response.EnsureSuccessStatusCode();
+        var responseData = await response.Content.ReadFromJsonAsync<JWTResponse>();
+        Assert.NotNull(responseData);
+        Assert.True(responseData.JWT.Length > 128);
+        Assert.True(responseData.RefreshToken.Length == Guid.NewGuid().ToString().Length);
+
+    }
+    
+    [Fact]
+    public async Task Login_Existing_User()
+    {
+        // Arrange
+        var loginData = new LoginInfo()
+        {
+            Email = "user@taltech.ee",
+            Password = "Foo.Bar.2"
+        };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/v1/account/login", loginData);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseData = await response.Content.ReadFromJsonAsync<JWTResponse>();
+        Assert.NotNull(responseData);
+        Assert.True(responseData.JWT.Length > 128);
+        Assert.True(responseData.RefreshToken.Length == Guid.NewGuid().ToString().Length);
     }
 
+    [Fact]
+    public async Task Login_Existing_User_Check_Rights()
+    {
+        // Arrange
+        var loginData = new LoginInfo()
+        {
+            Email = "user@taltech.ee",
+            Password = "Foo.Bar.2"
+        };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/v1/account/login", loginData);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseData = await response.Content.ReadFromJsonAsync<JWTResponse>();
+        Assert.NotNull(responseData);
+        Assert.True(responseData.JWT.Length > 128);
+        Assert.True(responseData.RefreshToken.Length == Guid.NewGuid().ToString().Length);
+        
+        
+        _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", responseData.JWT);
+        
+        var getResponse = await _client.GetAsync("/api/v1/persons");
+        getResponse.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task No_Bearer_Header_Unauthorized()
+    {
+        var getResponse = await _client.GetAsync("/api/v1/persons");
+        Assert.Equal(HttpStatusCode.Unauthorized, getResponse.StatusCode);
+    }
+    
+    [Fact]
+    public async Task JWT_Custom_Expiration()
+    {
+        // Arrange
+        var loginData = new LoginInfo()
+        {
+            Email = "user@taltech.ee",
+            Password = "Foo.Bar.2"
+        };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/v1/account/login?jwtExpiresInSeconds=2", loginData);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseData = await response.Content.ReadFromJsonAsync<JWTResponse>();
+        Assert.NotNull(responseData);
+        Assert.True(responseData.JWT.Length > 128);
+        Assert.True(responseData.RefreshToken.Length == Guid.NewGuid().ToString().Length);
+        
+        
+        _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", responseData.JWT);
+        
+        var getResponse = await _client.GetAsync("/api/v1/persons");
+        getResponse.EnsureSuccessStatusCode();
+
+        
+        // Wait for JWT to expire
+        await Task.Delay(3000);
+        var getResponseAuthExpired = await _client.GetAsync("/api/v1/persons");
+        
+        Assert.Equal(HttpStatusCode.Unauthorized, getResponseAuthExpired.StatusCode);
+    }
+
+
+    [Fact]
+    public async Task JWT_Refresh()
+    {
+        // Arrange
+        var loginData = new LoginInfo()
+        {
+            Email = "user@taltech.ee",
+            Password = "Foo.Bar.2"
+        };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/api/v1/account/login?jwtExpiresInSeconds=2", loginData);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var responseData = await response.Content.ReadFromJsonAsync<JWTResponse>();
+        Assert.NotNull(responseData);
+        Assert.True(responseData.JWT.Length > 128);
+        Assert.True(responseData.RefreshToken.Length == Guid.NewGuid().ToString().Length);
+        
+        
+        _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", responseData.JWT);
+        
+        var getResponse = await _client.GetAsync("/api/v1/persons");
+        getResponse.EnsureSuccessStatusCode();
+
+        
+        // Wait for JWT to expire
+        await Task.Delay(3000);
+
+        var getResponseAuthExpired = await _client.GetAsync("/api/v1/persons");
+        
+        Assert.Equal(HttpStatusCode.Unauthorized, getResponseAuthExpired.StatusCode);
+        
+        // Refresh JWT
+        var refreshResponse = await _client.PostAsJsonAsync("/api/v1/account/RenewRefreshToken", new RefreshTokenModel()
+        {
+            Jwt = responseData.JWT,
+            RefreshToken = responseData.RefreshToken
+        });
+        
+        var refreshedResponseData = await refreshResponse.Content.ReadFromJsonAsync<JWTResponse>();
+        Assert.NotNull(refreshedResponseData);
+        
+        
+        _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", refreshedResponseData.JWT);
+        
+        var getResponse2 = await _client.GetAsync("/api/v1/persons");
+        getResponse2.EnsureSuccessStatusCode();
+    }
+    
+    
 }
+
